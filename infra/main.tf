ecf5828e80d868fd5ff74764042d314a51a3a753
Rodrigo Kumabe <rodrigo.kumabe@gmail.com>
2023-12-28 18:03:18 -0300

    ci: ajustes no workflow

--- infra/main.tf
+++ infra/main.tf
@@ -1,43 +1,43 @@
 resource "aws_instance" "webapp" {
-    ami = var.imagem
-    instance_type = var.tipo_instancia
-    security_groups = [ "${aws_security_group.ssh_traffic.name}", "${aws_security_group.web_traffic.name}"]
-    user_data = file("./server_script.sh")
-    key_name = "id_rsa"
-    tags = {
-        Name = "webapp"
-    }
+  ami             = var.imagem
+  instance_type   = var.tipo_instancia
+  security_groups = ["${aws_security_group.ssh_traffic.name}", "${aws_security_group.web_traffic.name}"]
+  user_data       = file("./server_script.sh")
+  key_name        = "id_rsa"
+  tags = {
+    Name = "webapp"
+  }
 }
 
 resource "aws_security_group" "ssh_traffic" {
-    name = "Permitir acesso via ssh"
+  name = "Permitir acesso via ssh"
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "TCP"
-        cidr_blocks = ["201.43.58.24/32"]
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["201.43.58.24/32"]
+  }
 }
 
 resource "aws_security_group" "web_traffic" {
-    name = "Permitir acesso via web"
+  name = "Permitir acesso via web"
 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "TCP"
-        cidr_blocks = ["201.43.58.24/32"]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["201.43.58.24/32"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 output "webapp_public_ip" {
-    value = aws_instance.webapp.public_ip
+  value = aws_instance.webapp.public_ip
 }
